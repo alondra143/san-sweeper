f9820c8f9ac0e6aff71d0212ac8798967516a376
alondra 3 <not given>
2021-09-09 16:27:23 -0700

    added structure to html, css, js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// constant variables that won't change
+
+
+
+
+
+
+// state variables (do not assign values)
+
+
+
+
+
+
+// grab and cache elements from the DOM (look at html) within variables that need to be accessed more than once
+
+
+
+
+
+
+
+// event listeners
+
+
+
+// init function[remember to render() at the end], other functions[remember to render() at the end if it's updating something], render function
