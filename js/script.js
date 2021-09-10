5fdff9af9dc3dbad79656ef989f0f4a6fcd994ec
alondra 3 <not given>
2021-09-10 14:25:20 -0700

    updated styling, added more pseudocode

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 // constant variables that won't change
 
-
-
-
+//1. what constitutes a safe tile vs. a mine
+const tiles = {
+    safe: {
+        color: 'pink',
+    },
+    mine: {
+        color: 'purple',
 
 
 // state variables (do not assign values)
 
-
+let remainingTiles;
+let message;
 
 
 
@@ -21,6 +26,15 @@
 
 
 // event listeners
+
+//1. randomize the background color of buttons between pink and purple and hide it. 
+//     make an array of the two colors, 
+
+//2. when user clicks, reveal color of button.
+
+
+//3. if the button is pink, update the REMAINING TILES MESSAGE and subtract 1 from tile count and continue until it reaches 0, and then prompt a winner message. 
+// if it comes back as purple, reveal all purple tiles and prompt lose message
 
 
 
