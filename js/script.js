04624010969aa97edaaaa4798ff5819ea7a54673
alondra 3 <not given>
2021-09-11 16:46:09 -0700

    hid randomized colors, resets randomized colors, and able to view all mines git add .

--- js/script.js
+++ js/script.js
@@ -12,22 +12,62 @@ let message;
 
 
 // grab and cache elements from the DOM (look at html) within variables that need to be accessed more than once
-let tileBtns = document.querySelectorAll('#container > button');
+let tileBtns = document.querySelectorAll('#container > .tile');
 
+let coverUp = document.getElementsByClassName('default');
+
+
+
+document.querySelector('#restartBtn')
+    .addEventListener('click', resetGame);
+
+document.querySelector("#revealBtn")
+    .addEventListener('click', revealAll);
 
 // event listeners
 
 //1. randomize the background color of buttons between pink and purple and hide it. 
-for (let i = 0; i < tileBtns.length; i++) {
-    const randomColor = Math.floor(Math.random() * TILECOLORS.length);
-    tileBtns[i].style.backgroundColor = TILECOLORS[randomColor];
+
+function getColor() {
+    for (let i = 0; i < tileBtns.length; i++) {
+        const randomColor = Math.floor(Math.random() * TILECOLORS.length);
+        tileBtns[i].style.backgroundColor = TILECOLORS[randomColor];
+    };
 }
+
+
+
+function testingRemove() {
+    tileBtns[revealAll];
+};
 //2. when user clicks, reveal color of button.
+
+ function revealColor() {
+    oneBtn.removeAttribute('hidden');
+    oneCover.remove();
+ }
 
 
 //3. if the button is pink, update the REMAINING TILES MESSAGE and subtract 1 from tile count and continue until it reaches 0, and then prompt a winner message. 
 // if it comes back as purple, reveal all purple tiles and prompt lose message
+// if ( === '#a8a1c2') {
+//     showMines();
 
+// }
 
+// LOSER FUNCTION-REVEAL ALL TILES
 
+function revealAll() {
+    for (let i = 0; i < tileBtns.length; i++) {
+        tileBtns[i].removeAttribute('hidden')
+    for (let e = 0; e < coverUp.length; e++) {
+        coverUp[e].remove()
+    }
+    }
+};
 // init function[remember to render() at the end], other functions[remember to render() at the end if it's updating something], render function
+
+function resetGame() {
+    console.log('click is working');
+    getColor();
+}
